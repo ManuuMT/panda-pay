40bcfcb4e567766ffd8fee9425618d44306d3376
Emanuel Maldonado Tuero <emtuero@hotmail.com>
2024-09-16 01:56:30 +0200

    First font added

---
+++ src/app/components/Home/Hero.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function Hero() {
+    return (
+        <p className='text-3xl font-light'>We are in the payment solutions business and in the “caring for our customers” business. </p>
+    )
+}
