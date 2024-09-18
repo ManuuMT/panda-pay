22cd59199a8a429b8ec48cd70fcda1b7ecc58776
Emanuel Maldonado Tuero <emtuero@hotmail.com>
2024-09-18 04:04:01 +0200

    lenis implemented + hero html structured

--- src/app/components/Home/Hero.tsx
+++ src/app/components/Home/Hero.tsx
@@ -1,15 +1,29 @@
 import React from 'react'
+import heroImg from "@/assets/images/hero.png"
+import Image from 'next/image'
 
 export default function Hero() {
-    return (<>
-        <div className='editorial text-3xl'>
-            <p className='font-extralight'>We are in the payment solutions business and in the “caring for our customers” business. </p>
-            <p className='font-extralight italic'>We are in the payment solutions business and in the “caring for our customers” business. </p>
-            <p className=''>We are in the payment solutions business and in the “caring for our customers” business. </p>
-            <p className='italic'>We are in the payment solutions business and in the “caring for our customers” business. </p>
-            <p className='font-extrabold'>We are in the payment solutions business and in the “caring for our customers” business. </p>
-            <p className='font-extrabold italic'>We are in the payment solutions business and in the “caring for our customers” business. </p>
-        </div>
-    </>
+    return (
+        <main className='bg-white text-black flex flex-col items-center'>
+            <div className='mt-32 text-center'>
+                <h3 className='font-light text-5xl'>Hi! Our name is</h3>
+                <h1 className='altform font-bold text-[12rem]' style={{ lineHeight: "140px" }}>PandaPay</h1>
+            </div>
+            <div className='w-full h-[75vh]'>
+                <Image className='w-full h-full object-contain' src={heroImg} alt='hero' />
+            </div>
+            <div className='flex altform px-16 text-3xl my-16'>
+                <div className='w-1/4'>
+                    <p>Who we are</p>
+                </div>
+                <div className='w-3/4'>
+                    <div className='w-3/4'>
+                        <p>We’re in the payment solutions business – and in the “caring for our customers” business. For us, it’s not just about transactions, it’s about making life simpler for entrepreneurs, so we can help them succeed.</p>
+                        <p className='mt-4'>With PandaPay, what you see is what you get. No surprises, no fuss, no hidden fees or hidden agenda. It’s all there in black & white.</p>
+                        <button className='bg-black rounded-3xl text-white px-5 py-2 text-xs font-light mt-10'>Our Solutions</button>
+                    </div>
+                </div>
+            </div>
+        </main>
     )
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,21 @@
+'use client'
+
 import Hero from "@/app/components/Home/Hero";
+import { useEffect } from "react";
+import Lenis from '@studio-freight/lenis'
 
 export default function Home() {
+  useEffect(() => {
+    const lenis = new Lenis()
+
+    function raf(time: number) {
+      lenis.raf(time)
+      requestAnimationFrame(raf)
+    }
+
+    requestAnimationFrame(raf)
+  }, []);
+
   return (
     <>
       <Hero />
