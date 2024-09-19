9696f2fc6becc54b083efee0e336865347357f48
Emanuel Maldonado Tuero <emtuero@hotmail.com>
2024-09-19 17:18:20 +0200

    animations added to navbar and hero section

--- src/app/components/Home/Hero.tsx
+++ src/app/components/Home/Hero.tsx
@@ -1,39 +1,94 @@
-'use client'
+"use client";
 
-import heroImg from "@/assets/images/hero.png"
-import Image from 'next/image'
+import heroImg from "@/assets/images/hero.png";
 import { useRef } from "react";
-import { useScroll, useTransform, motion } from 'framer-motion';
+import { useScroll, useTransform, motion } from "framer-motion";
 
 export function Hero() {
-    const container = useRef(null);
-    const { scrollYProgress } = useScroll({
-        target: container,
-        offset: ['start start', 'end end']
-    })
-    const translate = useTransform(scrollYProgress, [0, 1], [0, -100]);
+  // * Hooks
+  const container = useRef(null);
+  const { scrollYProgress } = useScroll({
+    target: container,
+    offset: ["start start", "end end"],
+  });
+  const translate = useTransform(scrollYProgress, [0, 1], [0, -100]);
 
-    return (
-        <main ref={container} className='bg-white text-black flex flex-col items-center'>
-            <div className='mt-32 text-center'>
-                <h3 className='font-light text-5xl'>Hi! Our name is</h3>
-                <h1 className='altform font-bold text-[12rem]' style={{ lineHeight: "140px" }}>PandaPay</h1>
-            </div>
-            <motion.div className='w-full h-[75vh]' style={{ translateY: translate }}>
-                <Image className='w-full h-full object-contain' src={heroImg} alt='hero' />
+  const helloText = "Hi! Our name is".split(" ");
+  const variant = {
+    hidden: (custom: number) => ({ opacity: 0, y: custom }),
+    show: {
+      opacity: 1,
+      y: 0,
+      transition: {
+        duration: 0.5,
+      },
+    },
+  };
+
+  return (
+    <main
+      ref={container}
+      className="bg-white text-black flex flex-col items-center"
+    >
+      <div className="mt-32 text-center">
+        <h3 className="flex gap-2 justify-center">
+          {helloText.map((word, i) => (
+            <motion.div
+              key={word}
+              variants={variant}
+              initial="hidden"
+              animate="show"
+              custom={(i + 1) * 10}
+            >
+              <h3 className="font-light text-5xl">{word}</h3>
             </motion.div>
-            <div className='flex altform px-16 text-3xl my-24'>
-                <div className='w-1/4'>
-                    <p>Who we are</p>
-                </div>
-                <div className='w-3/4'>
-                    <div className='w-3/4'>
-                        <p>We’re in the payment solutions business – and in the “caring for our customers” business. For us, it’s not just about transactions, it’s about making life simpler for entrepreneurs, so we can help them succeed.</p>
-                        <p className='mt-4'>With PandaPay, what you see is what you get. No surprises, no fuss, no hidden fees or hidden agenda. It’s all there in black & white.</p>
-                        <button className='bg-black rounded-3xl text-white px-6 py-3 text-xs font-light mt-10'>Our Solutions</button>
-                    </div>
-                </div>
-            </div>
-        </main>
-    )
+          ))}
+        </h3>
+        <div className="overflow-hidden">
+          <motion.h1
+            className="altform font-bold text-[12rem]"
+            style={{ lineHeight: "140px" }}
+            initial={{ y: 100, opacity: 0 }}
+            animate={{ y: 0, opacity: 1 }}
+            transition={{ duration: 1, delay: 0.5 }}
+          >
+            PandaPay
+          </motion.h1>
+        </div>
+      </div>
+      <motion.div className="w-full h-[75vh]" style={{ translateY: translate }}>
+        <motion.img
+          initial={{ scale: 1.1, opacity: 0, filter: "blur(8px)" }}
+          animate={{ scale: 1, opacity: 1, filter: "blur(0px)" }}
+          transition={{ duration: 0.5, delay: 1 }}
+          className="w-full h-full object-contain"
+          src={heroImg.src}
+          alt="hero"
+        />
+      </motion.div>
+      <div className="flex altform px-16 text-3xl my-24">
+        <div className="w-1/4">
+          <p>Who we are</p>
+        </div>
+        <div className="w-3/4">
+          <div className="w-3/4">
+            <p>
+              We’re in the payment solutions business – and in the “caring for
+              our customers” business. For us, it’s not just about transactions,
+              it’s about making life simpler for entrepreneurs, so we can help
+              them succeed.
+            </p>
+            <p className="mt-4">
+              With PandaPay, what you see is what you get. No surprises, no
+              fuss, no hidden fees or hidden agenda. It’s all there in black &
+              white.
+            </p>
+            <button className="bg-black rounded-3xl text-white px-6 py-3 text-xs font-light mt-10">
+              Our Solutions
+            </button>
+          </div>
+        </div>
+      </div>
+    </main>
+  );
 }
--- src/app/components/Home/Navbar.tsx
+++ src/app/components/Home/Navbar.tsx
@@ -1,20 +1,53 @@
+import { motion } from "framer-motion";
+
 export function Navbar() {
-    return (
-        <div className="altform fixed top-0 z-10 text-black w-full">
-            <div className="flex w-full">
-                <div className="flex w-3/4">
-                    <div className="bg-black py-3 px-4 text-white flex justify-between w-2/5" style={{ borderRadius: "0 0 15px 0" }}>
-                        <p className="text-lg">PandaPay</p>
-                        <p>icon</p>
-                    </div>
-                    <div className="font-bold py-3 px-4">
-                        <p>Welcome</p>
-                    </div>
-                </div>
-                <div className="font-bold px-10 py-3 w-1/4 text-right">
-                    <p>Support</p>
-                </div>
-            </div>
+  const variant = {
+    hidden: { opacity: 0, y: -50 },
+    show: (custom: number) => ({
+      opacity: 1,
+      y: 0,
+      transition: {
+        duration: 0.5,
+        delay: custom,
+      },
+    }),
+  };
+
+  return (
+    <div className="altform fixed top-0 z-10 text-black w-full">
+      <div className="flex w-full">
+        <div className="flex w-3/4">
+          <motion.div
+            variants={variant}
+            initial="hidden"
+            animate="show"
+            custom={0}
+            className="bg-black py-3 px-4 text-white flex justify-between w-2/5"
+            style={{ borderRadius: "0 0 15px 0" }}
+          >
+            <p className="text-lg">PandaPay</p>
+            <p>icon</p>
+          </motion.div>
+          <motion.div
+            variants={variant}
+            initial="hidden"
+            animate="show"
+            custom={0.2}
+            className="font-bold py-3 px-4"
+          >
+            <p>Welcome</p>
+          </motion.div>
         </div>
-    )
+        <motion.div
+          variants={variant}
+          initial="hidden"
+          animate="show"
+          custom={0.4}
+          className="font-bold px-10 py-3 w-1/4 text-right"
+        >
+          <p>Support</p>
+        </motion.div>
+      </div>
+    </div>
+  );
 }
